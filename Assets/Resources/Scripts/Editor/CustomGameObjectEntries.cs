c965341a1d46d34ec7c8e0975ec67abde40af703
Marmex Mander <vadim.m.lutsk@gmail.com>
2024-07-18 17:49:58 +0300

    Added spawner creation button to editor

---
+++ Assets/Resources/Scripts/Editor/CustomGameObjectEntries.cs
@@ -0,0 +1,21 @@
+﻿using Heroicsolo.Scripts.Logics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Heroicsolo.Scripts.Editor
+{
+    public static class CustomGameObjectEntries
+    {
+        [MenuItem("GameObject/Game Logic/Spawner")]
+        public static void PlaceSpawner(MenuCommand command)
+        {
+            GameObject spawner = ObjectFactory.CreateGameObject("Spawner", typeof(MobsSpawner));
+            Utils.PrefabUtils.PlaceInViewport(spawner);
+        }
+    }
+}
