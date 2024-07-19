512b497db8710e13b02bd6aac35b22c615023d7b
Marmex Mander <vadim.m.lutsk@gmail.com>
2024-07-19 16:52:59 +0300

    Implemented IDictionary in WeightedChoser

--- Assets/Resources/Scripts/Utils/WeightedChoser.cs
+++ Assets/Resources/Scripts/Utils/WeightedChoser.cs
@@ -20,25 +20,43 @@ namespace Heroicsolo.Scripts.Utils
     }
 
     [Serializable]
-    public class WeightedChoser<T> : IEnumerable<ValueWeight<T>>
+    public class WeightedChoser<T> : IDictionary<T,float>
     {
 
-        private List<ValueWeight<T>> valuesWeights;
+        private Dictionary<T,float> valuesWeights;
         private List<float> comulateWeights;
         private bool isReady = false;
 
+        public ICollection<T> Keys => valuesWeights.Keys;
+
+        public ICollection<float> Values => valuesWeights.Values;
+
+        public int Count => valuesWeights.Count;
+
+        public bool IsReadOnly => false;
+
+        public float this[T key] 
+        {
+            get => valuesWeights[key]; 
+            set 
+            {
+                valuesWeights[key] = value;
+                isReady = false;
+            } 
+        }
+
         public WeightedChoser()
         {
-            valuesWeights = new List<ValueWeight<T>>();
+            valuesWeights = new Dictionary<T, float>();
         }
-        public WeightedChoser(IEnumerable<ValueWeight<T>> valuesWeights)
+        public WeightedChoser(IEnumerable<KeyValuePair<T,float>> valuesWeights)
         {
-            this.valuesWeights = new List<ValueWeight<T>>(valuesWeights);
+            this.valuesWeights = new Dictionary<T, float>(valuesWeights);
         }
         private void Prepare()
         {
-            valuesWeights = valuesWeights.OrderByDescending(i=>i.Weight).ToList();
-            comulateWeights = valuesWeights.Select(kv=>kv.Weight).ComulativeSum().ToList();
+            valuesWeights = new Dictionary<T, float>(valuesWeights.OrderByDescending(kv=>kv.Value));
+            comulateWeights = valuesWeights.Select(kv=>kv.Value).ComulativeSum().ToList();
             isReady = true;
         }
         public T Chose()
@@ -50,20 +68,70 @@ namespace Heroicsolo.Scripts.Utils
             while (true)
             {
                 if (comulateWeights[guess] > target)
-                    return valuesWeights.ElementAt(guess).Value;
+                    return valuesWeights.Keys.ToArray()[guess];
                 float hopDist = target - comulateWeights[guess];
-                guess += 1 + (int)(hopDist / valuesWeights.ElementAt(guess).Weight);
+                guess += 1 + (int)(hopDist / valuesWeights.Values.ToArray()[guess]);
             }
         }
 
-        public IEnumerator<ValueWeight<T>> GetEnumerator()
+        public void Add(T key, float value)
+        {
+            valuesWeights.Add(key, value);
+            isReady = false;
+        }
+
+        public bool ContainsKey(T key)
+        {
+            return valuesWeights.ContainsKey(key);
+        }
+
+        public bool Remove(T key)
+        {
+            isReady = false;
+            return valuesWeights.Remove(key);
+        }
+
+        public bool TryGetValue(T key, out float value)
+        {
+            return valuesWeights.TryGetValue(key, out value);
+        }
+
+        public void Add(KeyValuePair<T, float> item)
+        {
+            isReady = false;
+            ((ICollection<KeyValuePair<T, float>>)valuesWeights).Add(item);
+        }
+
+        public void Clear()
+        {
+            valuesWeights.Clear();
+            isReady = false;
+        }
+
+        public bool Contains(KeyValuePair<T, float> item)
+        {
+            return valuesWeights.Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<T, float>[] array, int arrayIndex)
+        {
+            ((ICollection<KeyValuePair<T, float>>)valuesWeights).CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(KeyValuePair<T, float> item)
+        {
+            isReady = false;
+            return ((ICollection<KeyValuePair<T, float>>)valuesWeights).Remove(item);
+        }
+
+        public IEnumerator<KeyValuePair<T, float>> GetEnumerator()
         {
             return valuesWeights.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return GetEnumerator();
+            return ((IEnumerable)valuesWeights).GetEnumerator();
         }
     }
 }
