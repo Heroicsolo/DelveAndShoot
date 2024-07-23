5a85068c75c5751112fa02a619544396a562ab0d
Marmex Mander <vadim.m.lutsk@gmail.com>
2024-07-23 16:14:17 +0300

    Fixed layout

--- Assets/Resources/Scripts/Editor/CustomInspectors/CharacterStat_Inspector.cs
+++ Assets/Resources/Scripts/Editor/CustomInspectors/CharacterStat_Inspector.cs
@@ -23,23 +23,26 @@ namespace Heroicsolo.Scripts.Editor.CustomInspectors
             var regenRate_prop = property.FindPropertyRelative("regenRate");
             var type_prop = property.FindPropertyRelative("statType");
             var baseValue_prop = property.FindPropertyRelative("baseValue");
+            var levelBonus_prop = property.FindPropertyRelative("bonusPerLevel");
             //Build UI
             VisualElement root = new();
-            var card = new VisualElement(); 
+            var card = new VisualElement();
             card.Add(new PropertyField(type_prop, "Type"));
             card.Add(new PropertyField(baseValue_prop, "Base Value"));
+            card.Add(new PropertyField(levelBonus_prop, "Bonus per Level"));
             //Build regen row
             var regen_checkbox = new PropertyField(isRegen_prop, "Regen");
-            var regenRate_field = new PropertyField(regenRate_prop, "Rate");
+            var regenRate_field = new PropertyField(regenRate_prop, "");
             var regen_box = new VisualElement();
-            regenRate_field.style.alignSelf = Align.FlexEnd;
             regen_box.style.flexDirection = FlexDirection.Row;
+            regen_box.style.alignItems = Align.FlexStart;
+            regenRate_field.style.flexGrow = 1;
             regen_box.Add(regen_checkbox);
             regen_box.Add(regenRate_field);
             card.Add(regen_box);
             root.Add(card);
             //Hide regen rate if unused
-            regenRate_field.visible = isRegen_prop.boolValue;
+            regenRate_field.SetEnabled(isRegen_prop.boolValue);
             return root;
         }
     }
