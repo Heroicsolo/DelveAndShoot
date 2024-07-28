bb66abf83d642d295cb20945e8b4cb3bb2496826
Marmex Mander <vadim.m.lutsk@gmail.com>
2024-07-28 15:47:37 +0300

    Enchanced row visuals and usage

---
+++ Assets/Resources/Scripts/UI/Utils/Row.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.UIElements;
+
+namespace Heroicsolo.Scripts.UI.Utils
+{
+    public class Row : ChildAnnotator
+    {
+        public Row(params VisualElement[] childs)
+        {
+            AddToClassList("row");
+            for (int i = 0; i < childs.Length; i++)
+                Add(childs[i]);
+        }
+    }
+}
