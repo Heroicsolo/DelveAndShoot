f35d68b6e97b03ede9c0ba378750fb32b5769a68
Marmex Mander <vadim.m.lutsk@gmail.com>
2024-08-04 15:57:49 +0300

    MultiselectDropbox progress

---
+++ Assets/Resources/Scripts/Editor/CustomInspectors/ItemParams_Inspector.cs
@@ -0,0 +1,30 @@
+﻿using Assets.FantasyInventory.Scripts.Data;
+using Assets.FantasyInventory.Scripts.Enums;
+using Heroicsolo.UI.UElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Heroicsolo.Scripts.Editor.CustomInspectors
+{
+    //[CustomPropertyDrawer(typeof(ItemParams))]
+    internal class ItemParams_Inspector : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            #region Props variables
+            var tagsProp = property.FindPropertyRelative("Tags");
+            #endregion
+            StyleSheet editorUSS = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Resources/UI/USS/Editor.uss");
+            VisualElement root = new();
+            #region Content
+
+            #endregion
+            root.styleSheets.Add(editorUSS);
+            return root;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/UI/UIelements/MultiselectDropbox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Heroicsolo.UI.UIelements
+{
+    internal class MultiselectDropbox<T> : BindableElement, INotifyValueChanged<List<T>>
+    {
+        private List<T> list;
+        private List<T> m_Value = new();
+        public List<T> value
+        {
+            get => m_Value;
+            set
+            {
+                if (value.Except(list).Count() <= 0)
+                    this.value = value;
+                else
+                    throw new ArgumentException("vales to select not exists in list");
+                if (panel != null)
+                    using (ChangeEvent<List<T>> evt = ChangeEvent<List<T>>.GetPooled(m_Value, value))
+                    {
+                        evt.target = this;
+                        SetValueWithoutNotify(value);
+                        SendEvent(evt);
+                    }
+            }
+        }
+
+        public void SetValueWithoutNotify(List<T> newValue)
+        {
+            m_Value = newValue;
+        }
+    }
+}
